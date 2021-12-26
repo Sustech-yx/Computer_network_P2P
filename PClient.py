65c26904a54b549c8cb3e154cd1fabb2c3db368a
zlrzz <75963909+zlrzz@users.noreply.github.com>
2021-12-27 03:40:38 +0800

    Update PClient.py

--- PClient.py
+++ PClient.py
@@ -86,24 +86,29 @@ class PClient:
                 # print("Total length of %s is %d bytes, %d packets" % (msg, len(data), len(packets)))
                 # self.proxy.sendto(len(packets), frm)
 
+                count1 = str(count)
+                while len(count1) < 6:
+                    count1 = '0' + count1
+
                 if fid1[0] in self.fid_addr_dict:
-                    self.proxy.sendto(self.try_to_send[fid1[0]][count], frm)
+                    sd = self.try_to_send[fid1[0]][count] + count1.encode()
+                    self.proxy.sendto(sd, frm)
                 else:
                     continue
             else:
                 # 正常传送
                 try:
+                    count1 = str(count)
+                    while len(count1) < 6:
+                        count1 = '0' + count1
                     if fid1[0] in self.fid_addr_dict:
-                        self.proxy.sendto(self.try_to_send[fid1[0]][count], frm)
+                        sd = self.try_to_send[fid1[0]][count] + count1.encode()
+                        self.proxy.sendto(sd, frm)
                     else:
                         continue
                 except Exception:
                     continue
                 # print(self.try_to_send[fid1[0]])
-
-
-
-
 
     def register(self, file_path: str):
         """
@@ -156,52 +161,110 @@ class PClient:
         count = 0
         stop = 1
         flag = False # 防止换人传时的重复传输
+        data = [None]
+        ack = [0]
+        q = [0]
 
-        while count < stop:
+        print(self.proxy.port, 'start transfer')
+        while ack:
+            start = time.time()
+            # print(start)
             self.__send__(fid_ori, self.tracker)
-            data_t = self.__recv__()
-            data_addr = data_t[0].decode()
-            if str(data_addr) == '[]':
+            try:
+                data_t = self.__recv__(2)
+            except TimeoutError:
+                print("tracker out")
                 continue
-            data_addr_1 = (str(data_addr[3:12]), int(data_addr[15:20]))
-            fid1 = fid + ' ' + str(count)
-            time.sleep(0.0001)
-            self.__send__(fid1.encode(), data_addr_1)
-            time.sleep(0.0001)
-            # print(msg)
-            if count == 0:
+            data_addr = data_t[0]
+            if str(data_addr) == '[]' or len(str(data_addr)) > 300:
+                # print('rubbish')
+                continue
+            index = 0
+            data_addr = str(data_addr.decode())
+            data_addr_1 = []
+            # print(data_addr)
+            while index + 20 < len(str(data_addr)):
+                data_addr_1.append((str(data_addr[index + 3:index + 12]), int(data_addr[index + 15:index + 20])))
+                index += 22
+            # print(self.proxy.port, data_addr_1)
+            while count == 0:
+                fid1 = fid + ' ' + str(count)
+                time.sleep(0.0001)
+                self.__send__(fid1.encode(), data_addr_1[0])
+                time.sleep(0.0001)
                 # 乞求第一个包
-                data = b""
+                data1 = b""
                 msg, frm = self.__recv__()
-                data += msg
+                data1 += msg
                 # print("%s receive %d" % (self.proxy.port, idx))
 
-                self.fid_addr_dict[fid] = data.decode()
-                data = b""
+                self.fid_addr_dict[fid] = data1.decode()
+                # print(self.fid_addr_dict, self.proxy.port)
                 msg, frm = self.__recv__()
-                stop = int(msg.decode())
-                msg, frm = self.__recv__()
-                data += msg
+                if len(msg) > 100:
+                    continue
+                stop = int(msg)
                 count += 1
-            while count < stop:
-                if flag:
-                    flag = False
-                    msg, frm = self.__recv__(5)
-                # 时间到了就换人
-                try:
-                    fid1 = fid + ' ' + str(count)
-                    self.__send__(fid1.encode(), data_addr_1)
-                    msg, frm = self.__recv__(5)
-                except Exception:
-                    # print('error')
-                    flag = True
+                ack = []
+                q = []
+                for i in range(count, stop):
+                    ack.append(i)
+                    q.append(i)
+                    data.append(None)
+                # print(q)
+                msg, frm = self.__recv__()
+                # msg = str(msg.decode())
+                msg1 = msg[-6:]
+                msg2 = msg[:-6]
+                data[int(msg1)] = msg2
+                for i in range(len(data_addr_1)):
+                    if not q:
+                        break
+                    k = q.pop()
+                    fid1 = fid + ' ' + str(k)
+                    self.__send__(fid1.encode(), data_addr_1[i])
+            else:
+                for i in range(len(data_addr_1)):
+                    if not q:
+                        break
+                    k = q.pop()
+                    fid1 = fid + ' ' + str(k)
+                    self.__send__(fid1.encode(), data_addr_1[i])
+                    # print(fid1)
+            while ack:
+                # print(self.proxy.port, ack)
+                if time.time() - start >= 70:
+                    q.clear()
+                    for i in range(len(ack)):
+                        q.append(ack[i])
                     break
-                data += msg
-                count += 1
-                print("\r", end="")
-                print(self.proxy.port, "Download progress: {}%: ".format(int(count / stop * 100)), "▋" * (int((count / stop * 100)) // 2), end="")
-                sys.stdout.flush()
-
+                try:
+                    msg, frm = self.__recv__(5)
+                    if frm == self.tracker:
+                        continue
+                    msg1 = msg[-6:]
+                    msg2 = msg[:-6]
+                    data[int(msg1)] = msg2
+                    if int(msg1) in ack:
+                        ack.remove(int(msg1))
+                    if q:
+                        k = q.pop()
+                        fid1 = fid + ' ' + str(k)
+                        self.__send__(fid1.encode(), frm)
+                    print("\r", end="")
+                    print(self.proxy.port, "Download progress: {}%: ".format(int((stop - (len(ack))) / stop * 100)),
+                          "▋" * (int(((stop - (len(ack))) / stop * 100)) // 2), end="")
+                    sys.stdout.flush()
+                except TimeoutError:
+                    # print('timeout')
+                    q.clear()
+                    # print(ack)
+                    for i in range(len(ack)):
+                        q.append(ack[i])
+                    break
+        data1 = b""
+        for i in range(len(data)):
+            data1 += data[i]
         self.active = True
         self.tthread = Thread(target=self.transfer_thread)
         self.tthread.start()
@@ -217,7 +280,8 @@ class PClient:
         """
         End of your code
         """
-        return data
+
+        return data1
 
     def cancel(self, fid):
         """
@@ -235,7 +299,6 @@ class PClient:
         fid = fid.encode()
         self.__send__(fid, self.tracker)
         time.sleep(0.0001)
-
         """
         End of your code
         """
@@ -245,22 +308,27 @@ class PClient:
         Completely stop the client, this client will be unable to share or download files any more
         :return: You can design as your need
         """
+        self.active = False
         time.sleep(0.0001)
-        for i in self.fid_addr_dict:
-            time.sleep(0.0001)
-            time.sleep(0.0001)
+        # print('close', self.fid_addr_dict, self.proxy.port)
+        for i, _ in self.fid_addr_dict.items():
             fid = i
-            time.sleep(0.0001)
+            # print(fid)
             fid = 'CANCEL:' + str(fid)
+            # print(fid)
             fid = fid.encode()
+            # print(fid)
             self.__send__(fid, self.tracker)
-            time.sleep(0.0001)
-            time.sleep(0.0001)
+            time.sleep(0.1)
+            self.__send__(fid, self.tracker)
+            time.sleep(0.1)
+            self.__send__(fid, self.tracker)
+            time.sleep(0.1)
+            self.__send__(fid, self.tracker)
         self.fid_addr_dict.clear()
         """
         End of your code
         """
-        self.active = False
         self.proxy.close()
 
 
